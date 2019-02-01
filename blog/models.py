2b80976d0db84f62f775d1e2272c2eec5f041e29
Shubham Pal <shubhampal1898@gmail.com>
2019-02-01 22:15:41 +0530

    commited

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#title
+#pub_date
+#image
+#body
+#add blog app to settings
+#create migrations
+#add app to admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    body = models.TextField()
